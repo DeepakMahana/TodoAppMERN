121c920197839de0dd0f4cb37a598a96d113ddef
Dcode10 <mahana.deepak20@gmail.com>
2020-09-03 21:21:34 +0530

    Ready for heroku deploy

---
+++ client/build/precache-manifest.baf498c5d12ae56d582c97b37ce44c97.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515953e91ce9ea710d653c0cb6de551b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e0b89e6c39c4c1c3828",
+    "url": "/static/css/2.48ce6163.chunk.css"
+  },
+  {
+    "revision": "bc718d034337f679bd74",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "3e0b89e6c39c4c1c3828",
+    "url": "/static/js/2.c6788138.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c6788138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc718d034337f679bd74",
+    "url": "/static/js/main.e02ab88f.chunk.js"
+  },
+  {
+    "revision": "052274ddd0fd19653d57",
+    "url": "/static/js/runtime-main.e13232da.js"
+  },
+  {
+    "revision": "2027659bd6f2ba5fde053e14c1024e60",
+    "url": "/static/media/TorreLogo.2027659b.png"
+  }
+]);
--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const http = require('http')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path')
 const log = require('./middleware/log')
+
 const app = express()
 
 // Load env from file for dev. Set NODE_ENV in your bashrc or zshrc.
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development' || 'production') {
   require('env2')('./devenv.json')
 }
 
@@ -18,13 +20,22 @@ app.use(bodyParser.json())
 app.use(bodyParser.raw())
 app.use(cors())
 
+// Serve static files from the React App
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 // Routers
 const userrouter = require('./routers/user')
 app.use('/api/user/', userrouter)
 const todorouter = require('./routers/todo')
+const { patch } = require('./routers/user')
 app.use('/api/todo/', todorouter)
 
-const port = process.env.SERVICE_PORT || 3000
+// Catch all handler for react app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+const port = process.env.SERVICE_PORT || 5000
 const server = http.createServer(app)
 server.listen(port, () => {
   log(`REST serving on port ${port}`.green.bold)
